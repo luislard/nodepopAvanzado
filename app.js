e28fb775ab7d43759b15d9ce8ec478e3e69b4b09
Luis Rosales <eng.luisrosales@gmail.com>
2017-09-19 22:53:45 +0200

    adding a console.log per request

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-
 var app = express();
 
 // view engine setup
@@ -14,6 +13,11 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(function(req,res,next){
+  console.log('hemos recibido una peticion');
+  next();
+});
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // conexion a la bbdd
 require('./lib/connectMongoose');
+require('./models/Advertisement');
 
 /**
  * lista de routers
