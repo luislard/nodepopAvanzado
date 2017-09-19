e4fb77ca7fdf95e5e96577b4cd7c9e8e2b552f24
Luis Rosales <eng.luisrosales@gmail.com>
2017-09-19 22:30:09 +0200

    error handler modified

--- app.js
+++ app.js
@@ -37,13 +37,35 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+
+  if (err.array) { // validation error
+    err.status = 422;
+    const errInfo = err.array({ onlyFirstError: true })[0];
+    err.message = isAPI(req) ?
+      { message: 'Not valid', errors: err.mapped()}
+      : `Not valid - ${errInfo.param} ${errInfo.msg}`;
+  }
+
+  res.status(err.status || 500);
+
+  // si es una petición al API respondo JSON...
+  if (isAPI(req)){
+    res.json({success: false, error: err.message});
+    return;
+  }
+
+  // y si no respondo con HTML
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
+  
   res.render('error');
 });
+
+function isAPI(req) {
+  return req.originalUrl.indexOf('/api') === 0;
+}
 
 module.exports = app;
