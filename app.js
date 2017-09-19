6022ab32e8d56dae9c53b1877fa50350226fdffb
Luis Rosales <eng.luisrosales@gmail.com>
2017-09-19 22:01:10 +0200

    adding connection to the app.js

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// conexion a la bbdd
+require('./lib/connectMongoose');
+
 app.use('/', index);
 app.use('/users', users);
 
