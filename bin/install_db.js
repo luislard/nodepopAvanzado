2b1d2a2891cde2729a8bd225a56c17ad4acd521e
Luis Rosales <eng.luisrosales@gmail.com>
2017-09-27 22:14:57 +0200

    changes to add tags to the db

--- bin/install_db.js
+++ bin/install_db.js
@@ -6,7 +6,9 @@
 require('../lib/connectMongoose');
 
 const Advertisement = require('../models/Advertisement');
-const data = require('../lib/advertisements.json');
+const Tag = require('../models/Advertisement');
+const data = require('../lib/data.json');
+
 
 Advertisement.deleteAll(function(){
     console.log('Borramos todos los advertisements existentes.');
@@ -22,6 +24,24 @@ for (let i = 0; i < data.anuncios.length; i++) {
     advertisement.save(function (err,savedAdvertisement) {
         if (err) throw err;
         console.log('Advertisement '+ savedAdvertisement.name+' was created');
+    });
+}
+
+
+Tag.deleteAll(function(){
+    console.log('Borramos todos los tags existentes.');
+});
+
+console.log('/*************/');
+console.log(data.tags.length);
+console.log('/*************/');
+
+for (let i = 0; i < data.tags.length; i++) {
+    const tag = new Advertisement(data.tags[i]);
+
+    tag.save(function (err,savedTag) {
+        if (err) throw err;
+        console.log('Advertisement '+ savedTag.name+' was created');
     });
 }
 
