acace99216801a2dd7d512b8a749149f261850a3
Luis Rosales <eng.luisrosales@gmail.com>
2017-11-02 19:29:10 +0100

    agreamos configuracion para usar mockgoose y conectarnos a la base de datos de testing

--- e2e/i18n.e2e.js
+++ e2e/i18n.e2e.js
@@ -1,9 +1,38 @@
 const request = require('supertest');
 
+// llamamos a la base de datos real
+var mongoose = require('mongoose');
+
+// Iniciamos mockgoose para crear la base de datos de testing
+var Mockgoose = require('mockgoose').Mockgoose;
+var mockgoose = new Mockgoose(mongoose);
+
 const app = require('../app');
 
 
 describe('home', function(){
+
+    before(async function(){ //
+        await mockgoose.prepareStorage();
+        await mongoose.connect('mongodb://example.com/TestingDb', {
+            useMongoClient: true
+        });
+        // limpiamos las definiciones de modelos y esquemas de mongoose
+        mongoose.models = {};
+        mongoose.modelSchemas = {};
+    });
+
+    // despues de cada test
+    afterEach(function(){
+
+    });
+
+    it('should return 200', function(done){
+        request(app)
+        .get('/')
+        .expect(200, done)
+    });
+    
     it('should return 200', function(done){
         request(app)
         .get('/')
